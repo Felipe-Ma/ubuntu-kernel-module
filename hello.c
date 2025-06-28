2fc890ef97de46998a0102fb357ae4adb3470a96
Felipe-Ma <martinezfa710@gmail.com>
2025-06-27 19:04:04 -0700

    initial commit

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+int init_module(void) {
+    printk(KERN_INFO "Hello, kernel!\n");
+    return 0;
+}
+
+void cleanup_module(void) {
+    printk(KERN_INFO "Goodbye, kernel!\n");
+}
+MODULE_LICENSE("GPL");
